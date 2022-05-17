36a30fa8b80e69ea925fdc09c41d081dd3a99dfe
Julio Moraes <juliocesardemoraes2015@gmail.com>
2022-05-16 22:14:22 -0300

    ajuste json de resposta dos endpoints

---
+++ src/main/java/com/client/ws/dto/error/ErrorResponseDTO.java
@@ -0,0 +1,17 @@
+package com.client.ws.dto.error;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class ErrorResponseDTO {
+
+    private String message;
+    private HttpStatus httpStatus;
+    private Integer statusCode;
+
+}
--- src/main/java/com/client/ws/exceptions/handlers/ResourceHandler.java
+++ src/main/java/com/client/ws/exceptions/handlers/ResourceHandler.java
@@ -1,5 +1,6 @@
 package com.client.ws.exceptions.handlers;
 
+import com.client.ws.dto.error.ErrorResponseDTO;
 import com.client.ws.exceptions.BadRequestException;
 import com.client.ws.exceptions.NotFoundException;
 import org.springframework.http.HttpStatus;
@@ -10,11 +11,21 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class ResourceHandler {
     @ExceptionHandler(NotFoundException.class)
-    public ResponseEntity<String> notFoundException(NotFoundException n) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(n.getMessage());
+    public ResponseEntity<ErrorResponseDTO> notFoundException(NotFoundException n) {
+        HttpStatus notFound = HttpStatus.NOT_FOUND;
+        return ResponseEntity.status(notFound).body(ErrorResponseDTO.builder()
+                .message(n.getMessage())
+                .httpStatus(notFound)
+                .statusCode(notFound.value())
+                .build());
     }
     @ExceptionHandler(BadRequestException.class)
-    public ResponseEntity<String> badRequestException(BadRequestException n) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(n.getMessage());
+    public ResponseEntity<ErrorResponseDTO> badRequestException(BadRequestException n) {
+        HttpStatus badRequest = HttpStatus.BAD_REQUEST;
+        return ResponseEntity.status(badRequest).body(ErrorResponseDTO.builder()
+                .message(n.getMessage())
+                .httpStatus(badRequest)
+                .statusCode(badRequest.value())
+                .build());
     }
 }
