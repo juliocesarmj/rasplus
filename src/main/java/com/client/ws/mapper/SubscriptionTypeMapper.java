5006867e2f62e49dc99da9c026f1a935b6d6d652
Julio Moraes <juliocesardemoraes2015@gmail.com>
2022-05-16 22:30:18 -0300

    adicao javax validation ao projeto

--- src/main/java/com/client/ws/mapper/SubscriptionTypeMapper.java
+++ src/main/java/com/client/ws/mapper/SubscriptionTypeMapper.java
@@ -5,6 +5,8 @@ import com.client.ws.model.SubscriptionType;
 
 public class SubscriptionTypeMapper {
 
+    private SubscriptionTypeMapper() {
+    }
     public static SubscriptionType fromDtoToEntity(SubscriptionTypeDto dto) {
         return SubscriptionType.builder()
                 .id(dto.getId())
